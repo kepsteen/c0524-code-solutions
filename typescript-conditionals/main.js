f68bece739632a0464226af9e8b3de686659ea74
Cody Epstein <codyepstein@gmail.com>
2024-05-16 16:53:39 +0000

    completed typescript-conditionals exercise

---
+++ typescript-conditionals/main.js
@@ -0,0 +1,72 @@
+'use strict';
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isOldEnoughToDrink(age) {
+  if (age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+function categorizeAcidity(pH) {
+  if (pH === 7) {
+    return 'neutral';
+  } else if (pH < 7 && pH >= 0) {
+    return 'acid';
+  } else if (pH <= 14 && pH > 7) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+}
+function introduceWarnerBro(name) {
+  name = name.toLowerCase();
+  switch (name) {
+    case 'yakko':
+      return "We're the Warner brothers!";
+    case 'wakko':
+      return "We're the Warner brothers!";
+    case 'dot':
+      return "I'm cute~";
+    default:
+      return 'Goodnight Everybody';
+  }
+}
+function recommendMovie(genre) {
+  switch (genre) {
+    case 'action':
+      return 'The Expendables';
+    case 'comedy':
+      return 'Superbad';
+    case 'horror':
+      return 'The Conjuring';
+    case 'musical':
+      return 'Hamilton';
+    case 'sci-fi':
+      return 'Planet of the Apes';
+    default:
+      return 'Choose one of the valid genres: action, comedy, horror, musical or sci-fi';
+  }
+}
