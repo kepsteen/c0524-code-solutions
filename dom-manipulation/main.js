5de456dda1e122d5096e445f6a9f05904de8d4db
Cody Epstein <codyepstein@gmail.com>
2024-05-28 21:45:22 +0000

    completed dom-manipulation exercise

---
+++ dom-manipulation/main.js
@@ -0,0 +1,25 @@
+'use strict';
+let clicks = 0;
+const $hotButton = document.querySelector('.hot-button');
+const $clicksTxt = document.querySelector('.click-count');
+if (!$hotButton) throw new Error('Element not found');
+if (!$clicksTxt) throw new Error('Element not found');
+function handleClick() {
+  if (!$hotButton || !$clicksTxt) {
+    throw new Error('The $hotButton or $clickCount query failed');
+  }
+  clicks++;
+  $clicksTxt.innerHTML = `Clicks: ${clicks}`;
+  if (clicks === 4) {
+    $hotButton.className = 'hot-button cool';
+  } else if (clicks === 7) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (clicks === 10) {
+    $hotButton.className = 'hot-button warm';
+  } else if (clicks === 13) {
+    $hotButton.className = 'hot-button hot';
+  } else if (clicks >= 16) {
+    $hotButton.className = 'hot-button nuclear';
+  }
+}
+$hotButton.addEventListener('click', handleClick);
