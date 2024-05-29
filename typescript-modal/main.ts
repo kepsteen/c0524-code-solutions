d58f672ef6ace9aa594d45ee86f3645eb460b3a6
Cody Epstein <codyepstein@gmail.com>
2024-05-29 21:50:27 +0000

    completed typescript-modal

---
+++ typescript-modal/main.ts
@@ -0,0 +1,13 @@
+const $openModal = document.querySelector('.open-modal');
+const $dismissModal = document.querySelector('.dismiss-modal');
+const $dialog = document.querySelector('dialog');
+
+if (!$openModal || !$dismissModal || !$dialog) {
+  throw new Error('dialog box not found');
+}
+
+$openModal.addEventListener('click', () => {
+  $dialog.showModal();
+});
+
+$dismissModal.addEventListener('click', () => $dialog.close());
