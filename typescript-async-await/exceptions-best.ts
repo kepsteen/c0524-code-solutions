f5281b42c0118b0d9764434dcec63a0bdeec3278
Cody Epstein <codyepstein@gmail.com>
2024-06-13 19:03:03 +0000

    test arguments changed

--- typescript-async-await/exceptions-best.ts
+++ typescript-async-await/exceptions-best.ts
@@ -7,12 +7,12 @@ const elapsed = (): string =>
   `${Math.round((Date.now() - startTime) / 1000)}s -`;
 
 async function throwOnce(): Promise<void> {
-  const msg = await read('foo', true);
+  const msg = await read('foo', false);
   console.log(elapsed(), 'throwOnce:', msg);
 }
 
 async function throwSeveral(): Promise<void> {
-  const msg1 = await read('foo1', true);
+  const msg1 = await read('foo1', false);
   console.log(elapsed(), 'throwSeveral1:', msg1);
   const msg2 = await read('foo2', false);
   console.log(elapsed(), 'throwSeveral2:', msg2);
@@ -21,7 +21,7 @@ async function throwSeveral(): Promise<void> {
 }
 
 async function throwChained(): Promise<void> {
-  const msg1 = await read('foo-chain', true);
+  const msg1 = await read('foo-chain', false);
   console.log(elapsed(), 'throwChained1:', msg1);
   const msg2 = await read(msg1, false);
   console.log(elapsed(), 'throwChained2:', msg2);
