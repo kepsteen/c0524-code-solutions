1ce9cf34a5254a65bda30ad62d1140b2104245be
Cody Epstein <codyepstein@gmail.com>
2024-06-13 19:11:59 +0000

    refactored function calls in await.ts to use await

--- typescript-async-await/await.ts
+++ typescript-async-await/await.ts
@@ -29,6 +29,6 @@ async function readChained(): Promise<void> {
   console.log(elapsed(), 'readChained3:', msg3);
 }
 
-readOnce()
-  .then(() => readSeveral())
-  .then(() => readChained());
+await readOnce();
+await readSeveral();
+await readChained();
