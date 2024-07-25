9d3b2de0e1c8025becdb019c2deaa3a122ccc7d2
Cody Epstein <codyepstein@gmail.com>
2024-07-25 21:29:27 +0000

    refactored useCart hook

--- react-context/my-app/src/components/useCart.ts
+++ react-context/my-app/src/components/useCart.ts
@@ -3,7 +3,7 @@ import { CartContext } from './CartContent';
 
 export function useCart() {
   const context = useContext(CartContext);
-  if (context) return context;
   if (!context)
     throw new Error('useCart can only be used within the CartProvider');
+  return context;
 }
