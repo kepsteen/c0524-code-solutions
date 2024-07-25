363e89cc1aa73e8621db21fff60763a6be21fe69
Cody Epstein <codyepstein@gmail.com>
2024-07-25 16:31:11 +0000

    changed div on product card to Link

--- full-stack-practice/client/src/components/ProductCard.tsx
+++ full-stack-practice/client/src/components/ProductCard.tsx
@@ -1,21 +1,15 @@
-import { useNavigate } from 'react-router';
 import { Product } from '../pages/Catalog';
 import { toDollars } from '../lib/to-dollars';
+import { Link } from 'react-router-dom';
 
 type Props = {
   product: Product;
 };
 
 export function ProductCard({ product }: Props) {
-  const navigate = useNavigate();
-  function handleClick(productId: number) {
-    navigate(`/products/${productId}`);
-  }
   return (
     <>
-      <div
-        className="card-wrapper"
-        onClick={() => handleClick(product.productId)}>
+      <Link to={`/products/${product.productId}`} className="card-wrapper">
         <div className="card">
           <div className="img-wrapper">
             <img
@@ -28,7 +22,7 @@ export function ProductCard({ product }: Props) {
           <p className="text-lg text-gray-400">{toDollars(product.price)}</p>
           <p>{product.shortDescription}</p>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
